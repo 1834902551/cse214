d890f70c9b7f99e2373ebfbbf8833f5cbac08cfe
Joty Akhter <65619869+1834902551@users.noreply.github.com>
2020-06-15 17:58:14 +0600

    Update 4.c

--- Lab1/4.c
+++ Lab1/4.c
@@ -5,8 +5,8 @@ int main()
 {
 
     int a[10] ={10, 20, 30, 40, 50, 60, 70, 80, 90, 100};
-    int x = 10;
-    int i;
+    int x = 10, i;
+    
     for(i=0;i<x;i++)
     {
         printf("%d\n", a[i]);
