59e8d203c4757ce8a399d870db9e9d332587f4c4
Joty Akhter <65619869+1834902551@users.noreply.github.com>
2020-06-15 18:09:18 +0600

    Update 1.c

--- Lab2/1.c
+++ Lab2/1.c
@@ -7,18 +7,18 @@ int main()
     int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
     int i, size = 10;
 
-    for(i=0; i<size; i++)
+    for(i=0;i<size;i++)
     {
-        printf("%d ", a[i]);
+        printf("%d ",a[i]);
     }
 
     printf("\nValue=11\n");
 
     a[9] = 11;
 
-    for(i=0; i<size; i++)
+    for(i=0;i<size;i++)
     {
-        printf("%d ", a[i]);
+        printf("%d ",a[i]);
     }
 
     return 0;
