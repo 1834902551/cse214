342f072a32f064f752b1764b88a6f2c1a147a9a4
Joty Akhter <65619869+1834902551@users.noreply.github.com>
2020-06-15 18:08:42 +0600

    Update 1.c

--- Lab2/1.c
+++ Lab2/1.c
@@ -8,14 +8,18 @@ int main()
     int i, size = 10;
 
     for(i=0; i<size; i++)
+    {
         printf("%d ", a[i]);
+    }
 
     printf("\nValue=11\n");
 
     a[9] = 11;
 
     for(i=0; i<size; i++)
+    {
         printf("%d ", a[i]);
+    }
 
     return 0;
 }
