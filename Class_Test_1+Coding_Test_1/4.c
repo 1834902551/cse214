1da5fa6922d1b6a20366dad7372ee1e0ace4cdf6
Joty Akhter <65619869+1834902551@users.noreply.github.com>
2020-08-06 16:32:51 +0600

    Add files via upload

---
+++ Class_Test_1+Coding_Test_1/4.c
@@ -0,0 +1,32 @@
+/* Copy the given array into another array. Access the array using pointer */
+
+#include<stdio.h>
+int main()
+{
+    int a[10] = {1, 2, 3, 4, 5, 6, 7, 8, 9, 10};
+    int b[10];
+    int size = 10, i, temp=0;
+    int *p, *q;
+
+    p = &a[0];
+    q = &b[0];
+
+    for(i=0; i<size; i++)
+    {
+        printf("%d ",*(p+i));
+    }
+    printf("\n");
+
+    for(i=0; i<size; i++)
+    {
+        temp = *(p+i);
+        *(q+i) = temp;
+    }
+
+    for(i=0; i<size; i++)
+    {
+      printf("%d ",*(p+i));
+    }
+
+    return 0;
+}
