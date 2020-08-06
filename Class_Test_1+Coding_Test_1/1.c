956964e03cf89f89395283dfe1d05ff589a7b2e1
Joty Akhter <65619869+1834902551@users.noreply.github.com>
2020-08-06 16:57:37 +0600

    Add files via upload

---
+++ Class_Test_1+Coding_Test_1/1.c
@@ -0,0 +1,31 @@
+#include<stdio.h>
+void array_scan_print(int arr[]);
+int main(){
+
+int arr[100];
+int i;
+
+ array_scan_print(arr[i]);
+
+return 0;
+}
+
+void array_scan_print(int arr[])
+{
+    printf("elements: \n");
+    int i;
+    for(i=0;i<=100;i++)
+    {
+        scanf("%d", &arr[i]);
+    }
+    if(i==100)
+    {
+        printf("\nOverflow");
+    }
+    for(i=0;i<=100;i++)
+    {
+        printf("%d", arr[i]);
+    }
+
+}
+
